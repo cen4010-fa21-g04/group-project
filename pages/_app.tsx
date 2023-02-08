a381a27f35546428a8577d008e75b89b00bd3df8
Steven <53283472+stevenluongo@users.noreply.github.com>
2023-02-08 12:59:06 -0500

    add stylesheets for each member

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
 import Layout from '@/components/layout';
+import '@/styles/about/brudowitz2021.css';
+import '@/styles/about/icosta2021.css';
+import '@/styles/about/jbadalcampbe2020.css';
+import '@/styles/about/sluongo2022.css';
+import '@/styles/about/sshank2018.css';
 import '@/styles/globals.css';
+
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
