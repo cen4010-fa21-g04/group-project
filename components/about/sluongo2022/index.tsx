a98b022bac39e946b6967fe6b6848aeb16013ea8
Steven <53283472+stevenluongo@users.noreply.github.com>
2023-02-08 13:26:21 -0500

    update sluongo2022 styles

--- components/about/sluongo2022/index.tsx
+++ components/about/sluongo2022/index.tsx
@@ -1,9 +1,10 @@
 export default function AboutSluongo2022() {
   return (
-    <div className="sluongo2022">
+    <div style={{ color: 'red' }} className="sluongo2022">
       <h1>Home</h1>
       <p className="sluongo2022-container">Sluongo02022</p>
       <p>About me: </p>
+      <p>Brief description about me</p>
     </div>
   );
 }
