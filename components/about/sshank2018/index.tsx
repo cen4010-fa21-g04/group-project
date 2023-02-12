8edc5216be88ae6d86813e37d9bb745ec2310aef
SadieS22 <sshank2018@fau.edu>
2023-02-12 11:08:39 -0500

    Update My Webpage
    Sadie Shank's About and CSS pages.

--- components/about/sshank2018/index.tsx
+++ components/about/sshank2018/index.tsx
@@ -1,3 +1,34 @@
 export default function AboutSshank2018() {
-  return <p>AboutSshank2018</p>;
+  return <><header><div>
+<br></br>
+    <h1 className="sshank2018h1"><b>Sadie</b> Shank</h1>
+  </div></header>
+  
+  <div> <br></br><div className="sshank2018about">
+   <br></br> <p className="sshank2018paragraph">I am a Senior at FAU dual majoring in Computer Science and History. I hope to work with software and web development once I graduate, including using technology like 3D modeling to design items because I enjoy the creative side of Computer Science.<br></br>
+<br></br>I am on a team for CEN 4010 with the following group members: Jaden Badal-Campbell, Isabela Costa, Steven Luongo, and Brian Rudowitz. </p>
+  </div></div>
+  
+  <div className = "sshank2018row">
+    <br></br>
+    <div className = "sshank2018column1">
+      
+      <h3 className = "sshank2018topic"><b>Education</b></h3>
+      <p>I am a Senior dual majoring in Computer Science and History with a Mathematics Minor and a Certificate in Data Science.</p>
+    </div>
+    <div className = "sshank2018column2">
+      <h3 className = "sshank2018topic"><b>Employment</b></h3>
+      <p>I have worked as a Methods of Calculus tutor at the Math Learning Center since August 2020.</p>
+      </div>
+    <div className = "sshank2018column3">
+      <h3 className = "sshank2018topic"><b>Hobbies</b></h3>
+      <p>In my free time, I enjoy crocheting, being outdoors, reading, and spending time with my family.</p>
+      </div>
+  
+  </div>
+  <div>
+    <h3 className="sshank2018contactheader"><br></br> <b>Contact</b></h3>
+    <p>Email:<b> sshank2018@fau.edu</b></p>
+  </div>
+  </> ;
 }
