9f4aeab3b0b1eaac03050547cf778c4e23697e40
Steven <53283472+stevenluongo@users.noreply.github.com>
2023-02-11 21:15:59 -0500

    update about webpage

--- components/about/sluongo2022/index.tsx
+++ components/about/sluongo2022/index.tsx
@@ -1,10 +1,3 @@
 export default function AboutSluongo2022() {
-  return (
-    <div style={{ color: 'red' }} className="sluongo2022">
-      <h1>Home</h1>
-      <p className="sluongo2022-container">Sluongo02022</p>
-      <p>About me: </p>
-      <p>Brief description about me</p>
-    </div>
-  );
+  return <h1 className="sluongo2022">Hello</h1>;
 }
