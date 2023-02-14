6e6497053a113a673a7a5da589ed697e03a2b025
icosta2021 <icosta2021@fau.edu>
2023-02-13 20:48:23 -0500

    icosta2021
    here's my stuff!

--- components/about/icosta2021/index.tsx
+++ components/about/icosta2021/index.tsx
@@ -2,24 +2,52 @@ export default function AboutIcosta2021()
 {
   return (
     <div className="icosta2021">
-      <p>Isabela Costa</p>
-      <p>Computer Science | Senior at FAU</p>
-      <p></p>
-      <p>Hello! Welcome to my about-me page.</p>
-      
-      <h4>Skills</h4>
-    <p>Enthusiastic new young adult, eager to contribute to team success through hard work, attention to detail and excellent organizational skills. Honesty about being a novice is always presented. A moldable, adaptable employee outlook willing to join your company. Motivated to learn, grow, and excel in your company. I've hosted numerous events that required careful planning and spanned over months in order to cultivate. Team player.</p>
-    <p>Languages: <a href="https://www.w3schools.com/cpp/cpp_intro.asp"> C++</a> and <a href="https://www.python.org/"> Python</a></p>
-    <p>Can handle the following: MS Office applications (Word, Excel, Outlook, Powerpoint)</p>
-    <p>MS OS troubleshooting</p>
-    <p>MS Windows 7/10 tools and programs</p>
-    <p>Inventory on office equipment (laser printers and copiers), cable management</p>
-    <p> </p>
-    <h4>Special Interests</h4>
-    <p>I'm currently working on getting a Certificate of Translation-- a document that states that I'm an expert in speaking a different language. I am working on getting it in Portuguese.</p>
-    <p>I've been working on a couple of projects in PHP, but I do not consider myself an expert just yet.</p>
-    <p>I frequently work with a Robotic's team that competes in <a href="https://www.firstinspires.org/robotics/frc"> FIRST</a> competitions. I am a mentor during their programming stages, and a mentor during the actual competitions.</p>
+      <html>
 
+<head>
+    <link rel="stylesheet" href="icosta2021.css" />
+    <title>Isabela Costa</title>
+</head>
+
+<body>
+
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <br></br>
+    <p className="icosta2021name"><span className="icosta2021highlight-pink">Isabela Costa</span></p> 
+    <p className="icosta2021intro"><p><i>Hello! My name is <span className="icosta2021thin-highlight-pink"><b>Isabela Costa</b></span> and I'm a <span className="thin-highlight-pink">Senior at FAU!</span> Welcome to my about-me page!</i></p></p>
+    
+    <div id="icosta2021projects" className="icosta2021pink-background">
+        <div className="icosta2021margin">
+            <h2 className="icosta2021section-title"><span className="icosta2021highlight-pink"><b>Resume</b></span></h2>
+            <p className="icosta2021proj-title"><span className="icosta2021thin-highlight-pink"><b>Skills</b></span></p>
+            <p className="icosta2021proj-text"><p>Enthusiastic new young adult, eager to contribute to team success through hard work, attention to detail and excellent organizational skills. Honesty about being a novice is always presented. A moldable, adaptable employee outlook willing to join your company. Motivated to learn, grow, and excel in your company. I've hosted numerous events that required careful planning and spanned over months in order to cultivate. Team player.</p>
+            <p>Languages: <span className="icosta2021thin-highlight-pink"><a href="https://www.w3schools.com/cpp/cpp_intro.asp"> C++</a></span> and <span className="icosta2021thin-highlight-pink"><a href="https://www.python.org/"> Python</a></span></p>
+            <p>Can handle the following: MS Office applications (Word, Excel, Outlook, Powerpoint)</p>
+            <p>MS OS troubleshooting</p>
+            <p>MS Windows 7/10 tools and programs</p>
+            <p>Inventory on office equipment (laser printers and copiers), cable management</p>
+            <p> </p></p>
+            <p className="icosta2021proj-title"><span className="icosta2021thin-highlight-pink"><b>Special Interests</b></span></p>
+            <p className="icosta2021proj-text"><p>I'm currently working on getting a Certificate of Translation-- a document that states that I'm an expert in speaking a different language. I am working on getting it in Portuguese.</p>
+            <p>I've been working on a couple of projects in PHP, but I do not consider myself an expert just yet.</p>
+            <p>I frequently work with a Robotic's team that competes in <span className="icosta2021thin-highlight-pink"><a href="https://www.firstinspires.org/robotics/frc"> FIRST</a></span> competitions. I am a mentor during their programming stages, and a mentor during the actual competitions.</p></p>
+        </div>
     </div>
+
+    <div id="icosta2021about-me" className="icosta2021white-background">
+        <div className="icosta2021margin">
+            <h2 className="icosta2021section-title"><span className="icosta2021highlight-pink">About Me (Truly!)</span></h2>
+            <p className="icosta2021proj-title"><span className="icosta2021thin-highlight-pink"><b>What do I do for fun?</b></span></p>
+            <p className="icosta2021proj-text">I play lots and lots of video games for fun! Lately, I've even gotten into streaming on my own. I play video games to relax, and I love sharing and conversing at the same time (though sometimes I can't do both without losing my train of thought). In an effort to combine them both, I usually put them together! Streaming is a delight. I also like to write on my free time; writing is one of my favorite hobbies. Reading and writing is usually how I spend my time whenever playing video games isn't appropriate for the moment.</p>
+        </div>
+    </div>
+                
+  </body>
+            
+  </html>
+  </div>
   );
 }
