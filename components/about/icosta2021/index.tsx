e4bde37f2f3e846adc3381b41450f4153ab86d2f
icosta2021 <icosta2021@fau.edu>
2023-02-13 19:29:15 -0500

    Update index.tsx

--- components/about/icosta2021/index.tsx
+++ components/about/icosta2021/index.tsx
@@ -1,3 +1,25 @@
-export default function AboutIcosta2021() {
-  return <p>icosta2021</p>;
+export default function AboutIcosta2021() 
+{
+  return (
+    <div className="icosta2021">
+      <p>Isabela Costa</p>
+      <p>Computer Science | Senior at FAU</p>
+      <p></p>
+      <p>Hello! Welcome to my about-me page.</p>
+      
+      <h4>Skills</h4>
+    <p>Enthusiastic new young adult, eager to contribute to team success through hard work, attention to detail and excellent organizational skills. Honesty about being a novice is always presented. A moldable, adaptable employee outlook willing to join your company. Motivated to learn, grow, and excel in your company. I've hosted numerous events that required careful planning and spanned over months in order to cultivate. Team player.</p>
+    <p>Languages: <a href="https://www.w3schools.com/cpp/cpp_intro.asp"> C++</a> and <a href="https://www.python.org/"> Python</a></p>
+    <p>Can handle the following: MS Office applications (Word, Excel, Outlook, Powerpoint)</p>
+    <p>MS OS troubleshooting</p>
+    <p>MS Windows 7/10 tools and programs</p>
+    <p>Inventory on office equipment (laser printers and copiers), cable management</p>
+    <p> </p>
+    <h4>Special Interests</h4>
+    <p>I'm currently working on getting a Certificate of Translation-- a document that states that I'm an expert in speaking a different language. I am working on getting it in Portuguese.</p>
+    <p>I've been working on a couple of projects in PHP, but I do not consider myself an expert just yet.</p>
+    <p>I frequently work with a Robotic's team that competes in <a href="https://www.firstinspires.org/robotics/frc"> FIRST</a> competitions. I am a mentor during their programming stages, and a mentor during the actual competitions.</p>
+
+    </div>
+  );
 }
