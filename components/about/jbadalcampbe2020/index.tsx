efa596e1b874dbb95e107a4b3542e8cec65ede72
Steven Christopher <53283472+stevenluongo@users.noreply.github.com>
2023-02-14 21:36:38 -0500

    fix linting errors

--- components/about/jbadalcampbe2020/index.tsx
+++ components/about/jbadalcampbe2020/index.tsx
@@ -1,42 +1,36 @@
 export default function AboutJbadalcampbe2020() {
-  return (<html>
-    <head>
-    </head>
+  return (
     <body className="jbadalcampbe2020-body">
-     <div>
-        <h1>HI, I'm Jaden Badal-Campbell</h1>
-     </div>
-     <br></br>
-    <div className="jbadalcampbe2020-row">
+      <div>
+        <h1>HI, Im Jaden Badal-Campbell</h1>
+      </div>
+      <br></br>
+      <div className="jbadalcampbe2020-row">
+        <div className="jbadalcampbe2020-column">
+          <h2 className="jbadalcampbe2020-h2-center">Who AM I</h2>
+          <p className="jbadalcampbe2020-p-center">
+            Computer Science major at FAU
+          </p>
+        </div>
 
-     <div className="jbadalcampbe2020-column">
-      <h2 className="jbadalcampbe2020-h2-center">Who AM I</h2>
-      <p className="jbadalcampbe2020-p-center">
-        Computer Science major at FAU
-      </p>
-    </div>
+        <div className="jbadalcampbe2020-column">
+          <h2 className="jbadalcampbe2020-h2-center">My Role</h2>
+          <p className="jbadalcampbe2020-p-center">
+            Hmmm, For now a developer but we will see......
+          </p>
+        </div>
 
-     <div className="jbadalcampbe2020-column">
-      <h2 className="jbadalcampbe2020-h2-center">My Role</h2>
-      <p  className="jbadalcampbe2020-p-center">
-        Hmmm, For now a developer but we will see......
-      </p>
-    </div>
-
-     <div className="jbadalcampbe2020-column">
-      <h2 className="jbadalcampbe2020-h2-center">HOBBIES</h2>
-      <p className="jbadalcampbe2020-p-center">
-        <ul>
-          <li>Piano</li>
-          <li>Video Games</li>
-          <li>Hobby Electronics</li>
-        </ul>
-      </p>
-    </div>
-
-     
-   </div> 
+        <div className="jbadalcampbe2020-column">
+          <h2 className="jbadalcampbe2020-h2-center">HOBBIES</h2>
+          <p className="jbadalcampbe2020-p-center">
+            <ul>
+              <li>Piano</li>
+              <li>Video Games</li>
+              <li>Hobby Electronics</li>
+            </ul>
+          </p>
+        </div>
+      </div>
     </body>
-  </html>
   );
- }
+}
--- components/about/sluongo2022/index.tsx
+++ components/about/sluongo2022/index.tsx
@@ -4,12 +4,15 @@ import IconButton from '@mui/material/IconButton';
 export default function AboutSluongo2022() {
   return (
     <div className="sluongo2022">
-      <img src="https://media.licdn.com/dms/image/D4E03AQEF_Yyc76WhxQ/profile-displayphoto-shrink_800_800/0/1673704399526?e=1681948800&v=beta&t=QkaIXQUXbz1v0Jr1BhXUCh06Em_2wGG1o7vXtZItugE" />
+      <img
+        src="https://media.licdn.com/dms/image/D4E03AQEF_Yyc76WhxQ/profile-displayphoto-shrink_800_800/0/1673704399526?e=1681948800&v=beta&t=QkaIXQUXbz1v0Jr1BhXUCh06Em_2wGG1o7vXtZItugE"
+        alt="steven luongo profile picture"
+      />
       <span className="sluongo2022-content">
-        <h1>Hello, I'm Steven Luongo</h1>
+        <h1>Hello, Im Steven Luongo</h1>
         <p>
-          I've been working as a professional developer for around 2 years now.
-          I enjoy building web apps and making peoples lives easier. Born and
+          Ive been working as a professional developer for around 2 years now. I
+          enjoy building web apps and making peoples lives easier. Born and
           raised in South Florida and currently attend FAU as a senior.
         </p>
         <span className="sluongo2022-links">
