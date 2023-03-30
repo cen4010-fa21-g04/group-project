9d8e0873a7ec00d3b5392602ae19593d57120d1a
Steven <53283472+stevenluongo@users.noreply.github.com>
2023-03-29 21:38:20 -0400

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    PROD_URL: process.env.PROD_URL,
+  },
+};
