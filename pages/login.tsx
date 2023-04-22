5864b4a4c575265d7e3faba51abde885737c020b
Steven <53283472+stevenluongo@users.noreply.github.com>
2023-04-22 09:59:13 -0400

    add firebase auth

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,29 +1,89 @@
 import Footer from '@/components/footer';
 import Header from '@/components/head';
-import Link from 'next/link';
+import { firebaseApp } from '@/firebase/firebaseApp';
+import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+const auth = getAuth(firebaseApp);
+
+const login = () => {
+  signInWithEmailAndPassword(auth, 'test@test.com', 'password');
+};
+const logout = () => {
+  signOut(auth);
+};
 
 export default function Login() {
+  const [user, loading, error] = useAuthState(auth);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  if (loading) {
+    return (
+      <div>
+        <p>Initialising User...</p>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div>
+        <p>Error: {error.message}</p>
+      </div>
+    );
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    try {
+      e.preventDefault();
+      signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (user) {
+    return (
+      <div>
+        <p>Current User: {user.email}</p>
+        <button onClick={logout}>Log out</button>
+      </div>
+    );
+  }
+
   return (
     <main className="layout_container">
       <Header title="Login" />
       <div className="login">
-        <form className="login_form">
+        <form onSubmit={handleSubmit} className="login_form">
           <h1>Log In - Employee View</h1>
           <span>
-            <input className = "lfirstform" type="text" placeholder="Username" />
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              className="lfirstform"
+              type="text"
+              placeholder="Username"
+            />
             <br></br>
-            <input className = "llastform" type="text" placeholder="Password" />
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              className="llastform"
+              type="text"
+              placeholder="Password"
+            />
             <br></br>
-            <button className = "lbutton" type="submit">Log In</button>
+            <button className="lbutton" type="submit">
+              Log In
+            </button>
           </span>
-          
         </form>
-        
-        <Link className = "outsidebutton" href="/reservations/list">
-            Quick Link Until Login Form is Setup
-          </Link>
       </div>
       <Footer href="main" title="Customer View" />
     </main>
   );
 }
+
+const CurrentUser = () => {
+  return <button onClick={login}>Log in</button>;
+};
